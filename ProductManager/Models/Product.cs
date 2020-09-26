2240d09695ce0abe446ea8043c907a9076f43876
longbot2703 <hoanglong2703@gmail.com>
2020-09-26 09:12:15 +0700

    fist commit

---
+++ ProductManager/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ProductManager.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        [JsonPropertyName("Img")]
+
+        public string Image { get; set; }
+
+        public string Name { get; set; }
+        public string Price { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    }
+}
---
+++ ProductManager/Services/JsonFileProductService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using ProductManager.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ProductManager.Services
+{
+    public class JsonFileProductService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+        private string jsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "Product.json"); }
+        }
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(jsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
